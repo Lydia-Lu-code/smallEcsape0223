15104abdd6fb8b065e2d8a0c631be4aab6dca823
Lydia-Lu-code <guipopo5@gmail.com>
2021-02-23 10:36:18 +0800

    Initial Commit

---
+++ smallEcsape0223/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smallEcsape0223
+//
+//  Created by 維衣 on 2021/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
